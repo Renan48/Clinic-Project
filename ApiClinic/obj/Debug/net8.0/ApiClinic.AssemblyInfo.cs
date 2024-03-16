a6260c085dad752b26d5a554d5dab544ec67586d
Renan <renan.p.silva@hotmail.com>
2024-03-16 20:40:33 -0300

    new folder

--- ApiClinic/obj/Debug/net8.0/ApiClinic.AssemblyInfo.cs
+++ ApiClinic/obj/Debug/net8.0/ApiClinic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiClinic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed75df9e3635e4cb9547ea154dc28c2c6266a653")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiClinic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiClinic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
